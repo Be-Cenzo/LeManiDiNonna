e422c685fdf15a3f5baf46500bee355adda42118
Be-Cenzo <vincenzooffertucci@gmail.com>
2021-09-06 17:32:21 +0200

    Database Aggiornato

--- db/create_progetto.sql
+++ db/create_progetto.sql
@@ -73,16 +73,23 @@ colore varchar(10) not null,
 descrizione varchar(30) not null,
 marca varchar(20),
 modello varchar(20),
-taglia varchar(20),
 imgurl varchar(50)
+);
+
+create table Taglie(
+prodotto int,
+taglia varchar(1),
+primary key (prodotto, taglia),
+foreign key (prodotto) references Prodotto(codice) on update cascade
 );
 
 create table Relato(
 prodotto int,
+taglia varchar(1),
 ordine int,
 quantità int not null,
-primary key (prodotto, ordine),
-foreign key (prodotto) references Prodotto(codice) on update cascade,
+primary key (prodotto, ordine, taglia),
+foreign key (prodotto, taglia) references Taglie(prodotto, taglia) on update cascade,
 foreign key (ordine) references Ordine(ID) on update cascade
 );
 
@@ -94,10 +101,11 @@ luogo varchar(30) not null
 create table Conservato(
 deposito int,
 prodotto int,
+taglia varchar(1),
 disponibilità int not null,
 primary key (deposito, prodotto),
 foreign key (deposito) references Deposito(ID) on update cascade,
-foreign key (prodotto) references Prodotto(codice) on update cascade
+foreign key (prodotto, taglia) references Taglie(prodotto, taglia) on update cascade
 );
 
 create table Fornitore(
@@ -151,33 +159,62 @@ insert into Deposito (luogo)
 values	("San Gennarello"),
 		("Mercanto San Severino");
 
-insert into Prodotto (tipo, prezzo, colore, descrizione, marca, modello, taglia, imgurl)
-values	("t-shirt", 15, "bianco", "Hakuna Matata T-Shirt", "hz", "normale", "S", "hakunamatata.jpg"),
-		("t-shirt", 15, "bianco", "Aereoplano T-Shirt", "hz", "normale", "S", "aereoplano.jpg"),
-		("t-shirt", 15, "nero", "Harry Potter T-Shirt", "hz", "normale", "S", "harrypotter.jpg"),
-		("t-shirt", 15, "bianco", "Acquarius T-Shirt", "hz", "normale", "S", "acquario.jpg"),
-		("t-shirt", 15, "bianco", "McDonalds T-Shirt", "hz", "normale", "S", "mcdonald.jpg"),
-		("t-shirt", 15, "nero", "Pavone T-Shirt", "hz", "normale", "S", "pavone.jpg"),
-		("t-shirt", 15, "blue", "Rock 'n Roll T-Shirt", "hz", "normale", "S", "rocknroll.jpg"),
-        ("prodotto", 8, "bianco", "Coffee Lover", null, null, null, "coffee-borsello.jpg"),
-        ("prodotto", 8, "bianco", "Renna", null, null, null, "renna-borsello.jpg"),
-        ("prodotto", 8, "bianco", "Rosa", null, null, null, "rosa-borsello.jpg"),
-        ("prodotto", 8, "nero", "Cloud", null, null, null, "cloud-cap.jpg"),
-        ("prodotto", 8, "nero", "Moon", null, null, null, "moon-cap.jpg"),
-        ("prodotto", 5, "bianca", "Disney Shopper", null, null, null, "disney-shopper.jpg"),
-        ("prodotto", 5, "bianca", "Peas Shopper", null, null, null, "peas-shopper.jpg"),
-        ("prodotto", 5, "bianca", "Cucina Bene Grembiule", null, null, null, "cucina-grembiule.jpg"),
-        ("prodotto", 5, "bianca", "Dogs Grembiule", null, null, null, "dogs-grembiule.jpg"),
-        ("abbigliamento", 25, "beige", "Rosa Felpa", "hz", "hoodie", "L", "rosa-felpa-beige.jpg"),
-        ("abbigliamento", 25, "grigio", "Live Ride Felpa", "hz", "hoodie", "L", "live.jpg");
+insert into Prodotto (tipo, prezzo, colore, descrizione, marca, modello, imgurl)
+values	("t-shirt", 15, "bianco", "Hakuna Matata T-Shirt", "hz", "normale", "hakunamatata.jpg"),
+		("t-shirt", 15, "bianco", "Aereoplano T-Shirt", "hz", "normale", "aereoplano.jpg"),
+		("t-shirt", 15, "nero", "Harry Potter T-Shirt", "hz", "normale", "harrypotter.jpg"),
+		("t-shirt", 15, "bianco", "Aquarius T-Shirt", "hz", "normale", "acquario.jpg"),
+		("t-shirt", 15, "bianco", "McDonalds T-Shirt", "hz", "normale", "mcdonald.jpg"),
+		("t-shirt", 15, "nero", "Pavone T-Shirt", "hz", "normale", "pavone.jpg"),
+		("t-shirt", 15, "blue", "Rock 'n Roll T-Shirt", "hz", "normale", "rocknroll.jpg"),
+        ("felpa", 25, "beige", "Rosa Felpa", "hz", "hoodie", "rosa-felpa-beige.jpg"),
+        ("felpa", 25, "grigio", "Live Ride Felpa", "hz", "hoodie", "live.jpg"),
+        ("borsello", 8, "bianco", "Coffee Lover", null, null, "coffee-borsello.jpg"),
+        ("borsello", 8, "bianco", "Renna", null, null, "renna-borsello.jpg"),
+        ("borsello", 8, "bianco", "Rosa", null, null, "rosa-borsello.jpg"),
+        ("cappello", 8, "nero", "Cloud", null, null, "cloud-cap.jpg"),
+        ("cappello", 8, "nero", "Moon", null, null, "moon-cap.jpg"),
+        ("shopper", 5, "bianca", "Disney Shopper", null, null, "disney-shopper.jpg"),
+        ("shopper", 5, "bianca", "Peas Shopper", null, null, "peas-shopper.jpg"),
+        ("grembiule", 5, "bianca", "Cucina Bene Grembiule", null, null, "cucina-grembiule.jpg"),
+        ("grembiule", 5, "bianca", "Dogs Grembiule", null, null, "dogs-grembiule.jpg");
+        
+insert into Taglie (prodotto, taglia)
+values  (1, "S"),
+		(1, "M"),
+		(1, "L"),
+		(2, "S"),
+		(2, "M"),
+		(2, "L"),
+		(3, "S"),
+		(3, "M"),
+		(3, "L"),
+		(4, "S"),
+		(4, "M"),
+		(4, "L"),
+		(5, "S"),
+		(5, "M"),
+		(5, "L"),
+		(6, "S"),
+		(6, "M"),
+		(6, "L"),
+		(7, "S"),
+		(7, "M"),
+		(7, "L"),
+		(8, "S"),
+		(8, "M"),
+		(8, "L"),
+		(9, "S"),
+		(9, "M"),
+		(9, "L");
 
 insert into Conservato 
-values	(1, 1, 15),
-		(2, 1, 30),
-        (2, 2, 40),
-        (1, 3, 12),
-        (1, 4, 27),
-        (2, 3, 20);
+values	(1, 1, "S", 15),
+		(2, 1, "M", 30),
+        (2, 2, "S", 40),
+        (1, 3, "L", 12),
+        (1, 4, "S", 27),
+        (2, 3, "S", 20);
 
 insert into Fornitore
 values	("Giuseppe Armenante"),
@@ -197,16 +234,16 @@ values	("2020-11-30 09:15:54", 63, 7, null, "Contabilizzato", "vincenzo.offertuc
         (current_timestamp(), 5, 10, null, "Contabilizzato", "vincenzo.offertucci@gmail.com", 1, "Bartolini");
 
 insert into Relato
-values	(1, 1, 3),
-		(2, 1, 3),
-        (1, 2, 2),
-        (2, 3, 2),
-        (3, 3, 3),
-        (3, 4, 3),
-        (4, 5, 4),
-        (2, 5, 2),
-        (1, 5, 1),
-		(4, 6, 1);
+values	(1, "S", 1, 3),
+		(2, "S", 1, 3),
+        (1, "M", 2, 2),
+        (2, "L", 3, 2),
+        (3, "S", 3, 3),
+        (3, "M", 4, 3),
+        (4, "S", 5, 4),
+        (2, "S", 5, 2),
+        (1, "L", 5, 1),
+		(4, "S", 6, 1);
 
 
 insert into fattura (data, ammontare, modalità, ordine)
